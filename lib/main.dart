88266d45f2de7b64f33877d7152aa3d4bb9ab5cc
MI ALI AFRASH <155898544+aliafrash@users.noreply.github.com>
2026-09-12 11:43:27 +0530

    Implement Firebase authentication foundation

--- lib/main.dart
+++ lib/main.dart
@@ -1,65 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+import 'firebase_options.dart';
 
 import 'core/theme/app_theme.dart';
-
 import 'core/routes/app_routes.dart';
-
 import 'screens/splash/splash_screen.dart';
-
 import 'screens/welcome/welcome_screen.dart';
 
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
 
-
-void main(){
-
-  runApp(
-    const CeylonAuraApp()
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
   );
 
+  runApp(const CeylonAuraApp());
 }
 
-
-
 class CeylonAuraApp extends StatelessWidget {
-
   const CeylonAuraApp({super.key});
 
-
   @override
-  Widget build(BuildContext context){
-
+  Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'CeylonAura',
+      theme: AppTheme.lightTheme,
+      initialRoute: AppRoutes.splash,
+      routes: {
+        AppRoutes.splash: (context) =>
+            const SplashScreen(),
 
-      debugShowCheckedModeBanner:false,
-
-
-      title:"CeylonAura",
-
-
-      theme:
-      AppTheme.lightTheme,
-
-
-      initialRoute:
-      AppRoutes.splash,
-
-
-      routes:{
-
-        AppRoutes.splash:
-        (context)=>
-        const SplashScreen(),
-
-
-        AppRoutes.welcome:
-        (context)=>
-        const WelcomeScreen(),
-
+        AppRoutes.welcome: (context) =>
+            const WelcomeScreen(),
       },
-
-
     );
-
   }
-
 }
